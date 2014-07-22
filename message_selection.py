1e0187112026606fd967b120e94b7cf00033db13
DiegoPereira <diegolimapereira@gmail.com>
2014-07-22 13:45:55 +0000

    Message manager

--- message_selection.py
+++ message_selection.py
@@ -1,8 +1,15 @@
-from messages.models import message_module
+from messages.models import Message
+import datetime
 
-def get_message_by_recipient(recipient):
-    ret = []
-    for message in message_module.objects.all():
-        if(message.recipient == recipient):
-            ret.append(message)
-    return ret
+
+class MessageManager:
+    def get_message_by_recipient(self, recipient):
+        ret = []
+        for message in Message.objects.all():
+            if(message.recipient == recipient):
+                ret.append(message)
+        return ret
+
+    def send_message(self, sender, recipient, subject, message):
+        m = Message(sender=sender, recipient=recipient, subject=subject,timestamp=datetime.datetime.now(), message=message, read='F')
+        m.save()
--- messages/models.py
+++ messages/models.py
@@ -1,12 +1,12 @@
 import sys
 from django.db import models
 
-class message_module(models.Model):
-    sender = models.CharField(max_length=30)
-    recipient = models.CharField(max_length=30)
-    subject = models.CharField(max_length=100)
+class Message(models.Model):
+    sender = models.CharField(max_length=100)
+    recipient = models.CharField(max_length=100)
+    subject = models.CharField(max_length=200)
     timestamp = models.DateTimeField('Data Published')
-    message = models.CharField(max_length=10000)
+    message = models.TextField(max_length=50000)
     read = models.CharField(max_length=1)
 
     def change_status(self, status):
