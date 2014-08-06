ee5e0010133749c73f8c689ed80af6944a238ddb
Caio-batista <caio.oliveira@ccc.ufcg.edu.br>
2014-08-06 17:26:24 +0000

    Table names changed

--- openstack_dashboard/dashboards/admin/recommendations/tables.py
+++ openstack_dashboard/dashboards/admin/recommendations/tables.py
@@ -48,12 +48,12 @@ class UpgradeFilterAction(tables.FilterAction):
 
 class UpgradeTable(tables.DataTable):
     host = tables.Column('host', verbose_name=_('Host'))
-    cpu_total = tables.Column('cpu_total', verbose_name=_('Total CPU'))
-    cpu_usage = tables.Column('cpu_usage', verbose_name=_('CPU usage'))
-    mem_total = tables.Column('memory_total', verbose_name=_('Total Memory'))
-    mem_usage = tables.Column('memory_usage', verbose_name=_('Memory usage'))
-    disk_total = tables.Column('disk_total', verbose_name=('Total Disk'))
-    disk_usage = tables.Column('disk_usage', verbose_name=_('Disk usage'))
+    cpu_total = tables.Column('cpu_total', verbose_name=_('Total number of CPUs'))
+    cpu_usage = tables.Column('cpu_usage', verbose_name=_('Number of CPUs used'))
+    mem_total = tables.Column('memory_total', verbose_name=_('Total Memory (MB)'))
+    mem_usage = tables.Column('memory_usage', verbose_name=_('Memory usage (MB)'))
+    disk_total = tables.Column('disk_total', verbose_name=('Total Disk (GB)'))
+    disk_usage = tables.Column('disk_usage', verbose_name=_('Disk used (GB)'))
     cpu_p = tables.Column('cpu_percentage', verbose_name=_('CPU (%)'))
     mem_p = tables.Column('memory_percentage', verbose_name=_('Memory (%)'))
     disk_p = tables.Column('disk_percentage', verbose_name=_('Disk (%)'))
