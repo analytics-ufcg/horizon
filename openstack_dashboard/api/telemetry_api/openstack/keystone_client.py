e11e513d0dee951a48988fb5f5e8a3b150a288cc
DiegoPereira <diegolimapereira@gmail.com>
2014-10-23 17:55:58 +0000

    remove keystone client hardcoded

--- openstack_dashboard/api/telemetry_api/openstack/keystone_client.py
+++ openstack_dashboard/api/telemetry_api/openstack/keystone_client.py
@@ -7,30 +7,34 @@ class KeystoneClient:
         self.__os_username = config.get('Openstack', 'osusername')
         self.__os_password = config.get('Openstack', 'ospassword')
         self.__os_auth_url = config.get('Openstack', 'osauthurl')
+        self.__admin_tenant_id = config.get('Openstack', 'AdminTenantId')
         self.__keystone = client.Client(username=self.__os_username, password=self.__os_password, auth_url=self.__os_auth_url)
         self.projects = [ {'name' : i.name, 'id' : i.id} for i in self.__keystone.tenants.list() ] 
         self.tenants = self.__keystone.tenants.list()
 
-    def get_user_email(self, user_id, project_id):
+    def get_user_email(self, user_id):
+        project_id = self.__admin_tenant_id
         keystone = client.Client(tenant_id=project_id, username=self.__os_username, password=self.__os_password, auth_url=self.__os_auth_url)
         userData = str(keystone.users.get(user_id))[6:-1]
         return ast.literal_eval(userData)['email']
 
-    def list_users(self, project_id='a67556c471d44dd58534a326098a4240'):
-        keystone = client.Client(tenant_id=project_id, username=self.__os_username, password=self.__os_password, auth_url=self.__os_auth_url)
+    def list_users(self):
+        keystone = client.Client(tenant_id=self.__admin_tenant_id, username=self.__os_username, password=self.__os_password, auth_url=self.__os_auth_url)
         users = []
         for user in keystone.users.list():
             users.append(user)
         return users
 
-    def get_user(self, project_id, name):
+    def get_user(self, name):
+        project_id=self.__admin_tenant_id        
         keystone = client.Client(tenant_id=project_id, username=self.__os_username, password=self.__os_password, auth_url=self.__os_auth_url)
         for user in keystone.users.list():
             if user.username == name:
                 return user
         return None
 
-    def get_username(self, user_id, project_id='a67556c471d44dd58534a326098a4240'):
+    def get_username(self, user_id):
+        project_id = self.__admin_tenant_id
         keystone = client.Client(tenant_id=project_id, username=self.__os_username, password=self.__os_password, auth_url=self.__os_auth_url)
         for user in keystone.users.list():
             if user.id == user_id:
@@ -38,7 +42,8 @@ class KeystoneClient:
         return None
 
 
-    def list_project_users(self, tenant_id, project_id='a67556c471d44dd58534a326098a4240'):
+    def list_project_users(self, tenant_id):
+        project_id = self.__admin_tenant_id
         keystone = client.Client(tenant_id=project_id, username=self.__os_username, password=self.__os_password, auth_url=self.__os_auth_url)
         users = keystone.tenants.list_users(tenant_id)
         users_list = []
@@ -49,6 +54,7 @@ class KeystoneClient:
         return users_list
 
 
-    def get_tenants(self, project_id='a67556c471d44dd58534a326098a4240'):
+    def get_tenants(self):
+        project_id = self.__admin_tenant_id
         keystone = client.Client(tenant_id=project_id, username=self.__os_username, password=self.__os_password, auth_url=self.__os_auth_url)
         return keystone.tenants.list()
