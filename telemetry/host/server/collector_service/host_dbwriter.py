67a92795134bf3c5486ebab15d91baf01c6c411b
Fábio Silva <fabio.fernando.osilva@gmail.com>
2014-11-12 15:54:02 +0000

    Removed default parameter values.

--- telemetry/host/server/collector_service/host_dbwriter.py
+++ telemetry/host/server/collector_service/host_dbwriter.py
@@ -3,7 +3,7 @@ import json
 
 class HostDataDBWriter:
 
-    def __init__(self, server='localhost', user='root', password='pass', db='hosts_data', table='hosts_data_table'):
+    def __init__(self, server, user, password, db, table):
         try:
             self.con = mdb.connect(server, user, password, db)
             self.table = table;
@@ -15,7 +15,6 @@ class HostDataDBWriter:
         cursor = self.con.cursor()
         try:
             query = "INSERT INTO %s (Date, Cpu_Util, Memory, Disk, Network, ServiceStatus, HostStatus, Host) VALUES(CURRENT_TIMESTAMP(), %f, '%s', '%s', '%s', '%s', '%s', '%s')" % (self.table, cpu, json.dumps(memory), json.dumps(disk), json.dumps(network), json.dumps(service_status), host_status, host)
-            #print query
             cursor.execute(query)
             self.con.commit()
             return "sucess"
