271f4ffe42b8d18b4758530dae45c390d1741799
Fábio Silva <fabio.fernando.osilva@gmail.com>
2014-12-03 18:39:45 +0000

    Using hosts configuration instead ComputeNodesMap (ComputeNodesMap -> Hosts)

--- openstack_dashboard/api/telemetry_api/start_bench_thread.py
+++ openstack_dashboard/api/telemetry_api/start_bench_thread.py
@@ -1,11 +1,13 @@
-import threading, ast, ConfigParser
+import threading
+import ast
+import host
 from benchmark_agent import start_bench_th
 
 def start_bench_(config):
-    hosts = ast.literal_eval(config.get('Openstack', 'computenodesmap'))
-    host_names =  hosts.keys()
-    for instance in host_names:
-        worker =  threading.Thread(target=start_bench_th, kwargs={'projeto':'admin', 'host': instance})
+    hosts = host.hosts_from_dict_list(ast.literal_eval(config.get('Openstack', 'Hosts')), 'compute_node')
+
+    for instance in hosts:
+        worker = threading.Thread(target=start_bench_th, kwargs={'projeto':'admin', 'host': instance.get_hostname()})
         worker.daemon = False
         worker.start()
     return 'thread started'
